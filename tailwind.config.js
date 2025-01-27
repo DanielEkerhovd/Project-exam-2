a5597d5184a34344b266749a62501957fd49a9cd
DanielEkerhovd <danielekerh@gmail.com>
2025-01-27 18:27:34 +0100

    Updated the project with some custom classes and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,15 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ["Inter", "sans-serif"],
       },
       colors: {
+        "holidaze-dark": "#292929",
+        "holidaze-light": "#FAF3E0",
+        "holidaze-highlight": "#FDB725",
       },
       screens: {
+        "2xl": "1440px"
       },
     },
   },
