674774c81c481753136f34b26649da790896ead3
DanielEkerhovd <danielekerh@gmail.com>
2025-02-13 16:31:42 +0100

    Setup of all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
+import { Venues } from "./pages/Venues";
+import { Venue } from "./pages/Venue";
+import { Profile } from "./pages/Profile";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
+
 import { Header } from "./components/Header";
 
 function App() {
@@ -9,6 +16,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/venues" element={<Venues />} />
+        <Route path="/venue/:id" element={<Venue />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,7 @@
-import { constants } from "../api/constants";
-import { useGetAPI } from "../api/apiCalls";
-
-import { VenueCard } from "../components/VenueCard";
-
 export function Home() {
-  const url =
-    constants.base + constants.holidaze.base + constants.holidaze.venues;
-
-  const { data, error, loading } = useGetAPI(url);
-
   return (
-    <>
-      <div className="w-11/12 max-w-screen-2xl mx-auto">
-        {loading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-        {data && (
-          <ul className="grid grid-cols-1 sm:grid-cols-2 gap-5">
-            {data.data.map((venue) => (
-              <VenueCard key={venue.id} {...venue} />
-            ))}
-          </ul>
-        )}
-      </div>
-    </>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
