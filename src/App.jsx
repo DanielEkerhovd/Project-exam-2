f5d16051017b4450e12a78a4361e107df0a1c2f2
DanielEkerhovd <danielekerh@gmail.com>
2025-02-14 13:32:06 +0100

    adding husky and eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import { Home } from "./pages/Home";
-import { Venues } from "./pages/Venues";
-import { Venue } from "./pages/Venue";
-import { Profile } from "./pages/Profile";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
+import { Home } from './pages/Home';
+import { Venues } from './pages/Venues';
+import { Venue } from './pages/Venue';
+import { Profile } from './pages/Profile';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
-
-import { Header } from "./components/Header";
+import { Header } from './components/Header';
 
 function App() {
   return (
